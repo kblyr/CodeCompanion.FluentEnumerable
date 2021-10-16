5bf133233eac1dabaef33419f9b605fe652bdc33
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-10-16 22:49:39 +0800

    IDictionaryExtensions

---
+++ src/FluentEnumerable/IDictionaryExtensions.cs
@@ -0,0 +1,17 @@
+namespace CodeCompanion.FluentEnumerable
+{
+    public static class IDictionaryExtensions
+    {
+        public static IDictionary<TKey, TValue> FluentAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            dictionary.Add(key, value);
+            return dictionary;
+        }
+
+        public static IDictionary<TKey, TValue> FluentAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, KeyValuePair<TKey, TValue> item)
+        {
+            dictionary.Add(item);
+            return dictionary;
+        }
+    }
+}
